a4cdb78ecb1c341686dd498216e5fbe6abc99e6b
nekusiku <nekusiku01@gmail.com>
2018-04-19 12:43:17 +0900

    time

---
+++ web/cgi-bin/nowdate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8-*-
+import datetime
+import time
+#import Tkinter
+
+html_body="""
+<html><body>
+%d/%d/%d %d:%d:%d
+</body></html>"""
+
+now=datetime.datetime.now()
+#seconds = datetime.timedelta(seconds)
+#while (seconds = seconds+1){
+def clock():
+    print "Content-type: text/html\n"
+    print html_body % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+clock().mainloop
+#}
---
+++ web/cgi-bin/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print "Content-type: text/html\n"
+print "<html><body>webapp</body></html>"
