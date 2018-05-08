03e35a70b9acc948d7f680af0efb8e9e707f2b5b
nekusiku <yuta8210@i.softbank.jp>
2018-05-08 13:10:01 +0900

    bet

--- DataSendI2C.py
+++ DataSendI2C.py
@@ -1,7 +1,12 @@
 import smbus
 import time
 import sys
+import RPi.GPIO as GPIO
 bus = smbus.SMBus(1)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.OUT)
+GPIO.setup(26,GPIO.OUT)
 
 SLAVE_ADDRESS = 0x0e
 reading = int(bus.read_byte(SLAVE_ADDRESS))
@@ -11,21 +16,20 @@ def request_reading():
     print(reading)
 
 while True:
-    #request_reading()
+    request_reading()
     command = input("Enter command: 1-Toggle LED, r-read A0:")
-    if command == 's':
+    if command == 's' and reading == 0:
         bus.write_byte(SLAVE_ADDRESS, ord('1'))
         print("send")
+        GPIO.output(26,1)
     elif command == 'r' and reading ==0:
-        request_reading()
         print("reading is none")
     elif command == 'r' and reading != 0:
-        request_reading()
+        #bus.write_byte(SLAVE_ADDRESS,ord('1'))
         print("reading is full")
+        GPIO.output(4,1)
     elif command == 'end':
         sys.exit();
-    elif command =='stop':
-        
     elif reading != 0:
         bus.write_byte(SLAVE_ADDRESS,ord('1'))
-    
+        
