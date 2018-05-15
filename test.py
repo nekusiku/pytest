9bc159319435a53fa8b5f79e91f9c5b819fbe224
nekusiku <yuta8210@i.softbank.jp>
2018-05-15 17:38:12 +0900

    te

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ GPIO.setup(26,GPIO.OUT)
 
 GPIO.output(4,0)
 GPIO.output(26,0)
-SLAVE_ADDRESS = 0x0e
+SLAVE_ADDRESS = 0x10
 register_SLAVE = 0x00
 
 def request_reading():
@@ -37,28 +37,31 @@ while True:
     reading = int(bus.read_byte(SLAVE_ADDRESS))
     reading=None
     if command == '1':
-        bus.write_byte(SLAVE_ADDRESS, ord('1'))
+        bus.write_byte(SLAVE_ADDRESS,'1')
         GPIO.output(4,1)
     elif command == 'r':
-        bus.write_byte(SLAVE_ADDRESS, ord('r'))
+        bus.write_byte(SLAVE_ADDRESS,ord('r'))
         request_reading()
         request_read_word()
         request_readingstr()
+        bus.read_byte_data(SLAVE_ADDRESS,register_SLAVE)
     elif command == 't':
         bus.write_byte(SLAVE_ADDRESS, ord('t'))
+        #bus.write_i2cblock_data(SLAVE_ADDRESS,ord(s[0]),ord(s[1:]))
         #request_reading()
         #bus.write_byte(SLAVE_ADDRESS,ord('t'))
         #print ("温度は"+str(bus.read_byte(SLAVE_ADDRESS,ord('t')))+"です")
         #print ("温度は"+reading<<10+"です")
         if bus.read_byte_data(SLAVE_ADDRESS,register_SLAVE)!=0:
             time.sleep(1)
-            print ("温度は"+str(bus.read_byte_data(SLAVE_ADDRESS,register_SLAVE)>>6)+"℃です")
+            print ("温度は"+str(bus.read_byte_data(SLAVE_ADDRESS,register_SLAVE))+"℃です")
 
         """elif (bus.write_byte(SLAVE_ADDRESS, ord('t'))==1):
             request_reading()
             if(request_reading()==1):
                 print(int(bus.read_byte(SLAVE_ADDRESS)))
                 long read_word_data(int addr,char cmd)"""
+        
     elif command =='end':
         bus.write_byte(SLAVE_ADDRESS, ord('e'))
         GPIO.cleanup()
