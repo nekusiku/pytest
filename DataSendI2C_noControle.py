de6bc9015d18b7a7f1a1a03eac66019540d4a789
nekusiku <yuta8210@i.softbank.jp>
2018-06-06 17:24:42 +0900

    tired

--- DataSendI2C_noControle.py
+++ DataSendI2C_noControle.py
@@ -11,27 +11,27 @@ GPIO.setup(26,GPIO.OUT)
 GPIO.output(4,0)
 GPIO.output(26,0)
 SLAVE_ADDRESS = 0x0e
-reading = int(bus.read_byte(SLAVE_ADDRESS))
+reading = int(bus.read_byte_data(SLAVE_ADDRESS,0x1c))
 
 def request_reading():
-    reading = int(bus.read_byte(SLAVE_ADDRESS))
+    reading = int(bus.read_byte_data(SLAVE_ADDRESS,0x1c))
     print(reading)
 
     
 while True:
     request_reading()
-    bus.write_byte(SLAVE_ADDRESS,ord('1'))
+    bus.write_byte_data(SLAVE_ADDRESS,0x1d,ord('1'))
     time.sleep(1)
     if(request_reading()!=0):
-        GPIO.output(4,1)
-        GPIO.output(26,0)
+        #GPIO.output(4,1)
+        #GPIO.output(26,0)
         #time.sleep(1)
         print("read")
-        bus.write_byte(SLAVE_ADDRESS,ord('1'))
-        if(bus.write_byte(SLAVE_ADDRESS,ord('1'))!=0):
+        bus.write_byte_data(SLAVE_ADDRESS,0x1d,ord('1'))
+        if(bus.write_byte_data(SLAVE_ADDRESS,0x1d,ord('1'))!=0):
             time.sleep(1)
-            GPIO.output(4,0)
-            GPIO.output(26,1)
+            #GPIO.output(4,0)
+            #GPIO.output(26,1)
             time.sleep(1)
             print("send")
     
