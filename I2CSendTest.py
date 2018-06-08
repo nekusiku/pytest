4f13d7b8475c8952d289961322a9828143667136
nekusiku <yuta8210@i.softbank.jp>
2018-06-08 17:30:00 +0900

    bes

--- I2CSendTest.py
+++ I2CSendTest.py
@@ -9,16 +9,16 @@ import sys
 
 bus = smbus.SMBus(1)#i2Cの設定
 SLAVE_ADDRESS = 0x0e#マイコンのi2Cアドレス、ここは絶対に変えない。
-register_read = 0x1e#読み込み用アドレス(マイコンの書き込み)
-register_write= 0x1e#書き込み用アドレス(マイコンの読み込み)
+register_read = 0x1d#読み込み用アドレス
+register_write= 0x1c#書き込み用アドレス
 
 def R_Read():
     #reading=int(bus.read_byte_data(SLAVE_ADDRESS,register_read))#指定されたアドレスのデータを１バイト読み取る
     #print(reading)
     read = int(bus.read_byte(SLAVE_ADDRESS))
     print(read)
-    print(bus.read_i2c_block_data(0x0e,register_write))
-    print(bus.read_i2c_block_data(0x0e,register_read))
+    print(bus.read_i2c_block_data(SLAVE_ADDRESS,register_write))
+    print(bus.read_i2c_block_data(SLAVE_ADDRESS,register_read))
     print("byte")
     print(bus.read_byte_data(SLAVE_ADDRESS,register_write))
     print(bus.read_byte_data(SLAVE_ADDRESS,register_read))
@@ -30,12 +30,12 @@ def D_Read():
     d_read=str(bus.read_byte(SLAVE_ADDRESS).decode())
     print(d_read)
 #read = int(bus.read_i2c_block_data(SLAVE_ADDRESS,register_SLAVE,10))
-bus.write_i2c_block_data(0x0e,0x1e,[0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00])
+#bus.write_i2c_block_data(0x0e,register_write,[0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00])
 while True:
     #bus.write_i2c_block_data(0x0e,0x1d,ord("1"))
     #print("初期化")
     #R_Read()
-    #bus.write_i2c_block_data(0x0e,register_write,[0x00,0x00,0x00,0x00,0x00,0x00])
+    bus.write_i2c_block_data(0x0e,register_write,[0x13])
     #print(bus.read_i2c_block_data(0x0e,0x01,7))
     #R_Read()
     #bus.write_byte_data(SLAVE_ADDRESS,register_write,ord("1"))
@@ -67,15 +67,22 @@ while True:
         time.sleep(1)
         R_Read()
     elif command == 'test':
+        bus.write_i2c_block_data(0x0e,register_write,[0x00,0x00,0x00,0x00,0x00,0x00])
+        bus.write_i2c_block_data(SLAVE_ADDRESS,register_write,[0x13])
+        print("sended")
+        #R_Read()
         #bus.write_i2c_block_data(0x0e,register_write,[0x13,0x14,0x15])
-        print('Test')
-        #bus.write_i2c_block_data(0x0e,register_write,[0x13,0x14,0x15])
-        bus.write_i2c_block_data(0x0e,0x1b,[0x01])
-        print(bus.read_i2c_block_data(0x0e,0x1b))
+        #print('Test')
+        
+        #bus.write_i2c_block_data(0x0e,register_write,[0x13])
+       
+        #print(bus.read_i2c_block_data(0x0e,register_write,3))
         #print(0x18)
-        time.sleep(1)
-        R_Read()
+        #time.sleep(1)
+        #R_Read()
+        print("end")
     elif command =='repeat':
+        bus.write_block_data(0x0e,register_write,[0x00,0x00,0x00,0x00,0x00,0x00])
         R_Read()
         time.sleep(1)
         R_Read()
---
+++ I2Ctest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import smbus
+import time
+import RPi.GPIO as GPIO
+import serial
+import sys
+
+bus = smbus.SMBus(1)#i2Cの設定
+SLAVE_ADDRESS = 0x0e#マイコンのi2Cアドレス、ここは絶対に変えない。
+register_read = 0x1d#読み込み用アドレス
+register_write= 0x1c#書き込み用アドレス
+
+while True:
+    bus.write_block_data(SLAVE_ADDRESS,register_write,[0x01])
