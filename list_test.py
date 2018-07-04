439f6cf13571b1bc0d1913e46fe3b2cf02f1e916
nekusiku <yuta8210@i.softbank.jp>
2018-07-04 09:19:50 +0900

    goodmorning

--- list_test.py
+++ list_test.py
@@ -9,7 +9,9 @@ import sys
 from enum import IntEnum
 
 read_list=[0x54,0x45,0x53,0x54]
+letter_list=["T","e","s","t"]
 T=(chr(read_list[0]))
 E=(chr(read_list[1]))
 S=(chr(read_list[2]))
-print(T+E+S+T)
+test.join(letter_list)
+print(test)
